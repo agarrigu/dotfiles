9270d5eea7816c7c9536007cbbc49b9bf81a3078
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-28 02:16:17 +0200

    add aliases for dotfiles git actions

--- .oh-my-zsh/custom/general.zsh
+++ .oh-my-zsh/custom/general.zsh
@@ -1,2 +1,8 @@
 alias asciiquarium= perl /usr/local/bin/asciiquarium
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dfa'/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add'
+alias dfau='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add --update'
+alias dfcmsg='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME commit -m'
+alias dfp='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME push'
+alias dfl='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull'
+
