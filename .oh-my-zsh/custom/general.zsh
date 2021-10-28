a7a46a81a6295a5ff21559d9558d265ca2570baf
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-28 14:40:45 +0200

    modify .todo.txt

--- .oh-my-zsh/custom/general.zsh
+++ .oh-my-zsh/custom/general.zsh
@@ -1,6 +1,6 @@
 alias asciiquarium= perl /usr/local/bin/asciiquarium
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias dfa'/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add'
+alias dfa='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add'
 alias dfau='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add --update'
 alias dfcmsg='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME commit -m'
 alias dfp='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME push'
