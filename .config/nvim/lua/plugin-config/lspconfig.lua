7f35645d1b1099d99ffba5c1dfa736ca6b2f039a
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-12-30 04:05:01 +0000

    remove unused languages and add rust in lspconfig.lua lspinstall.lua and treestitter.lua

--- .config/nvim/lua/plugin-config/lspconfig.lua
+++ .config/nvim/lua/plugin-config/lspconfig.lua
@@ -2,21 +2,15 @@ local nvim_lsp = require'lspconfig'
 local capabilites = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 nvim_lsp['vimls'].setup { capabilites = capabilites }
 nvim_lsp['bashls'].setup { capabilites = capabilites }
-nvim_lsp['dotls'].setup { capabilites = capabilites }
-nvim_lsp['cssls'].setup { capabilites = capabilites }
---nvim_lsp['jdtls'].setup { capabilites = capabilites }
 nvim_lsp['pyright'].setup { capabilites = capabilites }
-nvim_lsp['emmet_ls'].setup { capabilites = capabilites }
-nvim_lsp['sqlls'].setup { capabilites = capabilites }
---nvim_lsp['sumneko_lua'].setup {capabilites = capabilites }
+
 -- TODO fix `vim` to global variable
 nvim_lsp['sumneko_lua'].setup {
-    capabilites = capabilites,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 }
--- .config/nvim/lua/plugin-config/lspinstall.lua
+++ .config/nvim/lua/plugin-config/lspinstall.lua
@@ -2,23 +2,19 @@ local lsp_installer = require'nvim-lsp-installer'
 local servers = {
   'bashls',
   'vimls',
-  'dotls',
-  'cssls',
-  'emmet_ls',
   'pyright',
   'sumneko_lua',
-  'sqlls',
---  'jdtls',
+  'rust_analyzer',
 }
 
 for _, name in pairs(servers) do
-	local ok, server = lsp_installer.get_server(name)
-	if ok then
-		if not server:is_installed() then
-			print("Installing " .. name)
-			server:install()
-		end
-	end
+  local ok, server = lsp_installer.get_server(name)
+  if ok then
+    if not server:is_installed() then
+      print("Installing " .. name)
+      server:install()
+    end
+  end
 end
 
 lsp_installer.on_server_ready(function(server)
