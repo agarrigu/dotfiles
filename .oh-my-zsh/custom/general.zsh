b3d06877e6c7d1edf20eb68077f629c3efe06c84
alonsogmunoz <alonso.g.munoz@gmail.com>
2022-01-12 02:02:55 +0000

    add and configure yadm and removed dotfile aliases, updated readme.md

--- .oh-my-zsh/custom/general.zsh
+++ .oh-my-zsh/custom/general.zsh
@@ -1,12 +1,3 @@
-# Dotfiles git aliases 
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-alias dof="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-alias dfa="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add"
-alias dfau="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add --update"
-alias dfcmsg="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME commit -m"
-alias dfp="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME push"
-alias dfl="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull"
-
 # Aesthetic aliases
 alias asciiquarium= perl /usr/local/bin/asciiquarium
 alias random-figlet="figlet -f $(ls $(figlet -I2)/*.flf | shuf -n1)"
