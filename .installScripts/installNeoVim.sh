c3b9f58837960dd1e33987f21b29a547be641326
alonsogmunoz <alonso.g.munoz@gmail.com>
2022-01-14 16:12:21 +0100

    fix missing installNeovim.sh commands

--- .installScripts/installNeoVim.sh
+++ .installScripts/installNeoVim.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
 
 apt-get install gdebi fdfind ripgrep
-curl "https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz" -o "$HOME/Downloads"
-tar -zxf "$HOME/Downloads/nvim-linux64.tar.gz"
+curl "https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz" -o "$HOME"
+tar -zxf "$HOME/nvim-linux64.tar.gz"
+cp -a "$HOME/nvim-linux64/." "/usr"
+rm nvim-linux64 nvim-linux64.tar.gz
 
 # nvim dependecies, I think. It makes :checkhealth make more green checks.
 python -m install --user --ugrade pynvim
