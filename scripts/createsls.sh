53f00f4b2570f4140f454f14bdc7fc31553f26e7
agarrigu <83713047+agarrigu@users.noreply.github.com>
2025-04-13 20:20:26 +0100

    [scirpts] fortify createlns script

--- scripts/createsls.sh
+++ scripts/createsls.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-DH_FILES="
-.aliases
-.gdbinit
-.gitconfig
-.tmux.conf
-.vimrc
-.zsh_prompt
-.zshenv
-.zshrc
-"
+declare -ar DH_FILES=(
+".aliases"
+".gdbinit"
+".gitconfig	"
+".tmux.conf"	
+".vimrc	"
+".zsh_prompt	"
+".zshenv	"
+".zshrc	"
+)
 
-DH_DIRS="
-.vim
-.config/nvim
-"
+declare -ar DH_DIRS=(	
+".vim"	
+".config/nvim"	
+)
 
-for f in $DH_FILES; do
+for f in ${DH_FILES[@]}; do
 	[[ -f "$HOME/dotfiles/$f" ]] && [[ ! -f $HOME/$f ]] \
 	&& ln -s "$HOME/dotfiles/$f" "$HOME/$f"
 done
 
-for d in $DH_DIRS; do
+for d in ${DH_DIRS[@]}; do
 	[[ -d "$HOME/dotfiles/$d" ]] && [[ ! -d "$HOME/$d" ]] \
 	&& ln -s "$HOME/dotfiles/$d" "$HOME/$d"
 done
