f2da586e7a8dbcd5c56be8cac93409a81af732e6
agarrigu <83713047+agarrigu@users.noreply.github.com>
2025-04-13 20:20:25 +0100

    [nvim] fix ftplugin/c.lua

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -2,7 +2,9 @@
 local set = vim.opt_local
 set.tabstop = 4
 set.softtabstop = 4
+set.shiftwidth = 4
 set.textwidth = 80
 set.expandtab = false
 set.autoindent = true
+set.smarttab = true
 set.fileformat = "unix"
