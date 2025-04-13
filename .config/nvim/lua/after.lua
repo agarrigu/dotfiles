e064617cf756bfd677a3d3c38ef01fdfbf9fdc4f
agarrigu <83713047+agarrigu@users.noreply.github.com>
2025-04-13 20:20:26 +0100

    [nvim] change colorscheme to habamax

--- .config/nvim/lua/after.lua
+++ .config/nvim/lua/after.lua
@@ -1,7 +1,7 @@
 -- Colours
 vim.opt.background = 'dark'
 vim.highlight.on_yank({timeout=240})
-vim.cmd.colorscheme("apprentice")
+vim.cmd.colorscheme("habamax")
 vim.api.nvim_set_hl(0, "Normal", {ctermbg = "none"})
 vim.api.nvim_set_hl(0, "Whitespace", {ctermbg = "none"})
 vim.api.nvim_set_hl(0, "SignColumn", {ctermbg = "none"})
