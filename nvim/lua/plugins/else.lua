2026382f3bf1c7057d4b508d8a3baf6e279ab999
agarrigu <83713047+agarrigu@users.noreply.github.com>
2025-04-13 20:20:26 +0100

    remove annoying 42 thing

--- nvim/lua/plugins/else.lua
+++ nvim/lua/plugins/else.lua
@@ -1,15 +1,5 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	{
-		"vinicius507/norme.nvim",
-		dependencies = {"jose-elias-alvarez/null-ls.nvim"},
-		config = function ()
-			require("null-ls").setup({})
-			require("norme").setup({
-				cmd = os.getenv('HOME') .. '/bin/norminette'
-			})
-		end
-	},
+
 	-- Info
 	{
 		"nvim-lualine/lualine.nvim",
