0443e7b4742eca8a4e98457ca9cc88b70e6df3db
agarrigu <83713047+agarrigu@users.noreply.github.com>
2025-08-03 21:30:43 +0100

    [nvim] remove superfluous files

--- .config/nvim/lua/after.lua
+++
@@ -1,7 +0,0 @@
--- Colours
-vim.opt.background = 'dark'
-vim.highlight.on_yank({timeout=240})
-vim.cmd.colorscheme("habamax")
-vim.api.nvim_set_hl(0, "Normal", {ctermbg = "none"})
-vim.api.nvim_set_hl(0, "Whitespace", {ctermbg = "none"})
-vim.api.nvim_set_hl(0, "SignColumn", {ctermbg = "none"})
