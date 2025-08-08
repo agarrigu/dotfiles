795d264506eb65cda8a2d8f4140af94702bd5fc5
agarrigu <83713047+agarrigu@users.noreply.github.com>
2025-08-09 00:11:39 +0100

    [nvim] add and setup tree-sitter text-objects

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,10 +46,12 @@ vim.pack.add({
 	'https://github.com/sharkdp/fd',
 	'https://github.com/BurntSushi/ripgrep',
 	'https://github.com/nvim-treesitter/nvim-treesitter',
+	'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
 
 	-- Nice to have
 	'https://github.com/echasnovski/mini.pick',
 	'https://github.com/lewis6991/gitsigns.nvim',
+
 })
 
 -- LSP CONFIG --
@@ -75,6 +77,14 @@ require('nvim-toggler').setup()
 
 require('mini.pick').setup()
 
+-- Unbind conflicting vim text objects
+vim.keymap.set({ 'x', 'o' }, 'ia', '<nop>')
+vim.keymap.set({ 'x', 'o' }, 'aa', '<nop>')
+vim.keymap.set({ 'x', 'o' }, 'if', '<nop>')
+vim.keymap.set({ 'x', 'o' }, 'if', '<nop>')
+vim.keymap.set({ 'x', 'o' }, 'ie', '<nop>')
+vim.keymap.set({ 'x', 'o' }, 'ae', '<nop>')
+
 require'nvim-treesitter.configs'.setup {
 	ensure_installed = {
 		'asm',
@@ -97,9 +107,67 @@ require'nvim-treesitter.configs'.setup {
 	incremental_selection = { enable = true, },
 	indent = { enable = true, },
 	additional_vim_regex_highlighting = false,
+
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				['if'] = '@function.inner',
+				['af'] = '@function.outer',
+				['iC'] = '@call.inner',
+				['aC'] = '@call.outer',
+				['il'] = '@assignment.lhs',
+				['ir'] = '@assignment.rhs',
+				['iA'] = '@assignment.inner',
+				['aA'] = '@assignment.outer',
+				['ic'] = '@conditional.inner',
+				['ac'] = '@conditional.outer',
+				['in'] = '@number.inner',
+				['an'] = '@number.outer',
+				['ia'] = '@parameter.inner',
+				['aa'] = '@parameter.outer',
+				['ix'] = '@regex.inner',
+				['ax'] = '@regex.outer',
+				['ie'] = '@statement.inner',
+				['ae'] = '@statement.outer',
+				['is'] = '@class.inner',
+				['as'] = '@class.outer',
+			}
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+	},
 }
 
 -- PLUGIN MAPPINGS --
+
+-- Treesitter Textobjects
+local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 
 -- MiniPick
 local pick = require('mini.pick').builtin
